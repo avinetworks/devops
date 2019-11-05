731d3f5a6fffd3d35cd3b6bb19658a7074b01ec9
Rohan-sss1 <rohan.suryawanshi@avinetworks.com>
2019-11-05 16:27:04 +0530

    AV-72448 Added examples to configure health monitor properly

---
+++ terraform/avi_config/healthmonitors/variables.tf
@@ -0,0 +1,15 @@
+variable "avi_password" {
+  type    = string
+  default = "admin"
+}
+
+variable "avi_controller" {
+  type    = string
+  default = "10.10.x.x"
+}
+
+variable "avi_version" {
+  type    = string
+  default = "18.2.6"
+}
+
