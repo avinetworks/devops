db3553ef7fff316853403bb259cdd9befaee51a0
shubhamavi <shubham@avinetworks.com>
2020-01-02 16:33:27 +0530

    remove vendors, migrate to go mod, go based build
    update gitignore
    
    copy all controller response headers to proxy response
    
    comments
    
    add timeout setting for api requests
    
    minor
    
    minor remove retries
    
    remove redundant insecure

--- tools/avi-api-proxy/main.go
+++ tools/avi-api-proxy/main.go
@@ -12,40 +12,56 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"time"
 
-	"github.com/avinetworks/sdk/go/session"
+	"avi-api-proxy/session"
 	"github.com/golang/glog"
 	"github.com/julienschmidt/httprouter"
 )
+
+func proxyErrorResponse(w http.ResponseWriter, err *session.AviError) {
+	w.Header().Set("Content-Type", "application/json")
+	data := make(map[string]string)
+	data["error"] = fmt.Sprintf("%v", err)
+	if err.HttpStatusCode == 0 {
+		err.HttpStatusCode = 504
+		data["error"] = fmt.Sprintf("avi-api-proxy: %v", err)
+	}
+	w.WriteHeader(err.HttpStatusCode)
+	json.NewEncoder(w).Encode(data)
+}
 
 func proxyRequest(aviSession *session.AviSession, w http.ResponseWriter, r *http.Request) {
 	glog.Infof("[AVIPROXY]: Proxy request %s %s", r.Method, r.URL.RequestURI())
 	var err error
 	url := strings.TrimPrefix(r.URL.RequestURI(), "/")
 
-	// exported restRequest from goSDK, to get *http.Response as the response and not just []byte.
-	// Modified goSDK code in vendors/ dir
 	var payload interface{}
 	if err = json.NewDecoder(r.Body).Decode(&payload); err != nil && err != io.EOF {
 		glog.Errorf("[AVIPROXY]: Unable to decode payload - %v", err)
 	}
 
-	var resp *http.Response
-	if resp, err = aviSession.RestRequest(r.Method, url, payload, "admin"); resp == nil && err != nil {
-		glog.Errorf("[AVIPROXY]: REST request error - %v", err)
+	resp, avierror := aviSession.RestRequest(r.Method, url, payload, "admin")
+	if resp == nil && avierror != nil {
+		glog.Errorf("[AVIPROXY]: REST request error - %v", avierror)
+		// capture timeout and unreachable errors
+		proxyErrorResponse(w, avierror)
+		return
 	}
 
-	var data []byte
-	if data, err = ioutil.ReadAll(resp.Body); err != nil {
-		glog.Errorf("[AVIPROXY]: Unable to read response content - %v", err)
+	// copy all response headers as is, ignore set-cookie headers
+	for name, values := range resp.Header {
+		if name != "Set-Cookie" {
+			w.Header()[name] = values
+		}
 	}
+
+	// copy status code from controller response
+	w.WriteHeader(resp.StatusCode)
+
+	// copy response body from controller response
+	io.Copy(w, resp.Body)
 	resp.Body.Close()
-
-	contentTypeHeader := resp.Header.Get("Content-Type")
-	w.Header().Set("Content-Type", contentTypeHeader)
-	if _, err = w.Write(data); err != nil {
-		glog.Errorf("[AVIPROXY]: Couldn't write to response writer - %v", err)
-	}
 }
 
 func main() {
@@ -56,6 +72,7 @@ func main() {
 		version    = os.Getenv("AVI_VERSION")
 		tlsEnabled = os.Getenv("AVI_TLS_ENABLED")
 		caCertFile = os.Getenv("AVI_CACERT_FILE")
+		timeoutStr = os.Getenv("AVI_TIMEOUT")
 		port       = "8080"
 	)
 
@@ -65,9 +82,16 @@ func main() {
 	var err error
 	_, err = url.Parse(aviurl)
 	if err != nil {
-		glog.Errorf("[AVIPROXY]: Invalid URL provided: %s", err.Error())
+		glog.Errorf("[AVIPROXY]: Invalid URL provided: %v", err)
 		os.Exit(-1)
 	}
+
+	// Default timeout: 60 seconds
+	if timeoutStr == "" {
+		timeoutStr = "60s"
+	}
+	timeoutStr = fmt.Sprintf("%ss", timeoutStr)
+	timeout, _ := time.ParseDuration(timeoutStr)
 
 	var transport *http.Transport
 	tlsClientConfig := &tls.Config{InsecureSkipVerify: true}
@@ -92,11 +116,12 @@ func main() {
 	var aviSession *session.AviSession
 	aviSession, err = session.NewAviSession(aviurl, username,
 		session.SetPassword(password),
-		session.SetInsecure,
 		session.SetTransport(transport),
-		session.SetVersion(version))
+		session.SetVersion(version),
+		session.SetTimeout(timeout))
 	if err != nil {
 		glog.Errorf("Unable to initiate AviSession - %v", err)
+		os.Exit(-1)
 	}
 
 	defer aviSession.Logout()
