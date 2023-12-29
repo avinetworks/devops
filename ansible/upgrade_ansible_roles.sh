30a5b47d8578f4c539add57424abadcf520ecf97
Nick Robbins <17030717+nrobbins00@users.noreply.github.com>
2023-12-29 10:28:28 -0600

    Adjust names of folders

--- ansible/upgrade_ansible_roles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Please enter role name without 'avinetworks.' "
-read -p "Enter Role Name:" role_name
-
-ansible-galaxy remove avinetworks.${role_name}
-ansible-galaxy install avinetworks.${role_name}
