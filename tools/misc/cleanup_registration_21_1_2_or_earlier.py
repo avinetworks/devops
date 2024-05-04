c7f95e9ae113dba5308a34b9059455845e6ddd36
Abhinav Modi <abhinavmodi@gmail.com>
2024-05-04 12:58:54 +0530

    cloud console deregistration scripts

---
+++ tools/misc/cleanup_registration_21_1_2_or_earlier.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#!/usr/bin/python3
+############################################################################
+# ========================================================================
+# Copyright 2024 VMware, Inc. All rights reserved. VMware Confidential
+# ========================================================================
+###
+
+import sys, os, django
+
+sys.path.append('/opt/avi/python/bin/portal')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal.settings_full')
+
+from api.models import ControllerPortalRegistration
+
+django.setup()
+
+if __name__ == '__main__':
+    for cpr in ControllerPortalRegistration.objects.all():
+        cpr.delete()
+    os.system("systemctl restart license_mgr")
+    os.system("systemctl restart portalconnector")
+
