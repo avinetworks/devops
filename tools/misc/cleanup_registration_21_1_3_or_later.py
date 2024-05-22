0cfd8930d55d3294c0618cc862666214520e8895
Janakiram Yellapu <janakiram.yellapu@broadcom.com>
2024-05-22 13:15:17 +0530

    Update cleanup_registration_21_1_3_or_later.py

--- tools/misc/cleanup_registration_21_1_3_or_later.py
+++ tools/misc/cleanup_registration_21_1_3_or_later.py
@@ -13,7 +13,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal.settings_full')
 
 import yaml
 from api.models import ControllerPortalRegistration
-from api.models import LicenseStatus # Comment out for versions lower than 21.1.3
 from api.models import ControllerLicense
 from avi.infrastructure.datastore import Datastore
 
@@ -37,41 +36,58 @@ def get_controller_version():
         print(f"Error parsing YAML: {err}")
         return ""
 
+def is_version_less_than(version, target_version):
+    version_parts = version.split('.')
+    target_parts = target_version.split('.')
+    
+    if len(version) == 0 or len(version) != 3 :
+        return False
+
+    for i in range(min(len(version_parts), len(target_parts))):
+        if int(version_parts[i]) < int(target_parts[i]):
+            return True
+        elif int(version_parts[i]) > int(target_parts[i]):
+            return False
+    
+    return False
 
 if __name__ == '__main__':
     ds = Datastore()
 
     for cpr in ControllerPortalRegistration.objects.all():
         cpr.delete()
+    print("Controller's registration state is cleared.")
     
     controller_version = get_controller_version()
     print(f"Controller version is : {controller_version}")
 
-    if controller_version >= "21.1.3":
+    if is_version_less_than(controller_version, "21.1.3") :
+         print("Controller version is less than 21.1.3 no license clean up needed.")
+    else:
+        from api.models import LicenseStatus
+
         # Delete license status object
         for ls in LicenseStatus.objects.all():
             ls.delete() 
 
         # Delete only virtual licenses from the database
         for controllerLicense in ControllerLicense.objects.all():
-            filteredLicenses = []
-            for license in controllerLicense.json_data['licenses']:
-                if(license['license_type'] != "Virtual"):
-                    filteredLicenses.append(license)
+            if 'licenses' in controllerLicense.json_data:
+                filteredLicenses = []
+                for license in controllerLicense.json_data['licenses']:
+                    if 'license_type' in license and license['license_type'] != "Virtual" :
+                        filteredLicenses.append(license)
 
-            controllerLicense.json_data['licenses'] = filteredLicenses
-            
-            
-            # Save the modified license back to the database
-            controllerLicense.save()
-            controllerLicense.save_pb()
+                controllerLicense.json_data['licenses'] = filteredLicenses
+                
+                # Save the modified license back to the database
+                controllerLicense.save()
+                controllerLicense.save_pb()
 
-            ds.save('controllerlicense', controllerLicense.json_data['uuid'], controllerLicense.protobuf(), None)
+                ds.save('controllerlicense', controllerLicense.json_data['uuid'], controllerLicense.protobuf(), None)
 
-
-        # Delete all licenses from datastore (Deleting licenses from datastore deleting licenses from DB also)
-
+        print("Controller's license state is cleared.")
+       
     os.system("systemctl restart license_mgr")
     os.system("systemctl restart portalconnector")
-    print("Cleaned up registration state")
 
