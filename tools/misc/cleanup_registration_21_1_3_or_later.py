c954a91c81b124f183686385b5133bc9f4d96bad
Janakiram Yellapu <janakiram.yellapu@broadcom.com>
2024-05-23 12:11:57 +0530

    Changed print statements to work in any python version.

--- tools/misc/cleanup_registration_21_1_3_or_later.py
+++ tools/misc/cleanup_registration_21_1_3_or_later.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-#!/usr/bin/python3
 ############################################################################
 # ========================================================================
 # Copyright 2024 VMware, Inc. All rights reserved. VMware Confidential
 # ========================================================================
 ###
 
-import sys, os, django
+import sys
+import os
+import django
 
 sys.path.append('/opt/avi/python/bin/portal')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal.settings_full')
@@ -18,14 +19,14 @@ from avi.infrastructure.datastore import Datastore
 
 django.setup()
 
-
 VERSION_FILE_PATH = "/bootstrap/VERSION"
+
 def get_controller_version():
     try:
         with open(VERSION_FILE_PATH, 'r') as file:
             ctrlr_props = file.read()
     except IOError as err:
-        print(f"Error while opening version file. Error: {err}")
+        print("Error while opening version file. Error: {}".format(err))
         return ""
 
     try:
@@ -33,15 +34,20 @@ def get_controller_version():
         controller_version = c.get("Version", "")
         return controller_version
     except yaml.YAMLError as err:
-        print(f"Error parsing YAML: {err}")
+        print("Error parsing YAML: {}".format(err))
         return ""
 
 def is_version_less_than(version, target_version):
+    # If the version of the controller is invalid, then it's
+    # better not to clean up virtual licenses so returning True.
+    if len(version) == 0:
+        return True
+    
     version_parts = version.split('.')
     target_parts = target_version.split('.')
     
-    if len(version) == 0 or len(version) != 3 :
-        return False
+    if len(version_parts) != 3:
+        return True
 
     for i in range(min(len(version_parts), len(target_parts))):
         if int(version_parts[i]) < int(target_parts[i]):
@@ -59,27 +65,27 @@ if __name__ == '__main__':
     print("Controller's registration state is cleared.")
     
     controller_version = get_controller_version()
-    print(f"Controller version is : {controller_version}")
+    print("Controller version is : {}".format(controller_version))
 
-    if is_version_less_than(controller_version, "21.1.3") :
-         print("Controller version is less than 21.1.3 no license clean up needed.")
+    if is_version_less_than(controller_version, "21.1.3"):
+         print("Controller version is less than 21.1.3, no license clean up needed.")
     else:
         from api.models import LicenseStatus
 
         # Delete license status object
         for ls in LicenseStatus.objects.all():
-            ls.delete() 
+            ls.delete()
 
         # Delete only virtual licenses from the database
         for controllerLicense in ControllerLicense.objects.all():
             if 'licenses' in controllerLicense.json_data:
                 filteredLicenses = []
                 for license in controllerLicense.json_data['licenses']:
-                    if 'license_type' in license and license['license_type'] != "Virtual" :
+                    if 'license_type' not in license or license['license_type'] != "Virtual":
                         filteredLicenses.append(license)
 
                 controllerLicense.json_data['licenses'] = filteredLicenses
-                
+
                 # Save the modified license back to the database
                 controllerLicense.save()
                 controllerLicense.save_pb()
@@ -90,4 +96,3 @@ if __name__ == '__main__':
        
     os.system("systemctl restart license_mgr")
     os.system("systemctl restart portalconnector")
-
